65756f346bfd497d7bd67caffc9fe0cdd73eacc9
Angel Olazabal <angeljoq26@gmail.com>
2024-05-27 14:36:27 -0500

    cambiando el nombre

--- saludo.js
+++ saludo.js
@@ -2,4 +2,4 @@ const saludo = (name)=>{
     let res = `Hola amigo ${name} hiciste la tare de pw2?, como estas, hace tiempo que no te veo jajaj hola`
 }
 
-saludo("Angel");
+saludo("Angel Olazabal Quille");

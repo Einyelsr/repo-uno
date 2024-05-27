414c6a0c561ea9e74ef35b287b080ff12be27a3a
Angel Olazabal <138731188+Einyelsr@users.noreply.github.com>
2024-05-27 15:23:33 -0500

    Update saludo.js

--- saludo.js
+++ saludo.js
@@ -2,4 +2,4 @@ const saludo = (name)=>{
     let res = `Hola amigo ${name} hiciste la tare de pw2?, como estas, hace tiempo que no te veo jajaj hola`
 }
 
-saludo("Paris Jackson");
+saludo("Angelito lito");
